fc5050fbe0fe9f7a2dad16586f0aa74654d1557a
joyanta3619 <akashshom3619@gmail.com>
2025-09-03 00:52:13 +0600

    web automation update xptah

--- Web_Automation/src/test/java/org/example/InteractWithWebElement.java
+++ Web_Automation/src/test/java/org/example/InteractWithWebElement.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
@@ -36,16 +37,34 @@ public class InteractWithWebElement extends WebSetUp {
         Thread.sleep(2000);
 
 
-        autocomplete_inputbox.clear();
-        Thread.sleep(20000);
+//        autocomplete_inputbox.clear();
+//        Thread.sleep(2000);
 
-        autocomplete_inputbox = browser.findElement(By.xpath("//input[@id='q']"));
+        // clear এর জায়গায় CTRL+A + DELETE ব্যবহার করো
+
+        autocomplete_inputbox.sendKeys(Keys.CONTROL + "a");
+        autocomplete_inputbox.sendKeys(Keys.DELETE);
+        Thread.sleep(2000);
 
         autocomplete_inputbox.sendKeys("BANGLADESH");
         Thread.sleep(2000);
 
 
 
+
+       //attribute er value dekhar jnno like id,class
+
+       String id = autocomplete_inputbox.getAttribute("id");
+       System.out.println("ID: "+ id);
+
+       String valueOfAttribute = autocomplete_inputbox.getAttribute("placeholder");
+       System.out.println("Attribute Value: "+valueOfAttribute);
+
+
+
+       WebElement  LoadMore = browser.findElement(By.xpath("//div[@class='load-more-button']"));
+       String backgroundColour = LoadMore.getCssValue("cursor");
+        System.out.println(backgroundColour);
 
     }
 
