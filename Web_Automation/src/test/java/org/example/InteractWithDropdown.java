b1d27e0de3d0645083757119ad4fc5c6eab72d5b
joyanta3619 <akashshom3619@gmail.com>
2025-09-04 01:54:46 +0600

    web automation update xptah

--- Web_Automation/src/test/java/org/example/InteractWithDropdown.java
+++ Web_Automation/src/test/java/org/example/InteractWithDropdown.java
@@ -1,7 +1,12 @@
 package org.example;
 
-import net.bytebuddy.implementation.bytecode.Throw;
+//import com.sun.tools.javac.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class InteractWithDropdown extends WebSetUp{
 
@@ -11,8 +16,25 @@ public class InteractWithDropdown extends WebSetUp{
         browser.get("https://rahulshettyacademy.com/AutomationPractice/");
         Thread.sleep(1000);
 
+
+    WebElement dropDown = browser.findElement(By.id("dropdown-class-example"));
+
+    Select select = new Select(dropDown);
+
+    List<WebElement> options = select.getOptions();
+
+    for(WebElement option : options){
+        System.out.println(option.getText());
+    }
+
+
+    select.selectByContainsVisibleText("Select");
+    Thread.sleep(2000);
+    System.out.println(select.getFirstSelectedOption().getText());
+
     }
 
 }
 
 
+//26.49
