2006121c9e1d69b7b5971f859adcbbcddf02f76d
joyanta3619 <akashshom3619@gmail.com>
2025-09-10 18:13:40 +0600

    web automation update xptah

---
+++ Web_Automation/src/test/java/org/example/HandleFrameContent.java
@@ -0,0 +1,53 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+import javax.swing.*;
+
+public class HandleFrameContent extends WebSetUp{
+    @Test
+    public void testFrameContent() throws InterruptedException {
+
+        browser.get("https://rahulshettyacademy.com/AutomationPractice/");
+
+
+        Actions actions = new Actions(browser);
+        actions.scrollByAmount(0,1200).build().perform();
+        Thread.sleep(2000);
+
+
+        browser.switchTo().frame("courses-iframe");
+
+        WebElement email = browser.findElement(By.xpath("//li[normalize-space()='contact@rahulshettyacademy.com']"));
+        System.out.println(email.getText());
+
+
+
+        browser.switchTo().defaultContent();     //outside iframe tai default a switch kore nilam
+
+        WebElement text = browser.findElement(By.xpath("//legend[normalize-space()='iFrame Example']"));
+        System.out.println(text.getText());
+
+
+
+//        browser.switchTo().frame("iframe-name");         //again switch to iframe
+
+        browser.switchTo().frame(0);                        //iframe index number use kore
+        WebElement  iFreameLogin = browser.findElement(By.xpath("//a[normalize-space()='Login']"));
+        System.out.println(iFreameLogin.getText());
+
+
+
+
+
+
+
+    }
+}
+
+
+
+//23
